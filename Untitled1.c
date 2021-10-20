ae3e944ffb8f32601aeb52187b766299f3f98c42
Yattik Sihag <31979752+yattik@users.noreply.github.com>
2021-10-20 11:29:35 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void sort()
+{
+    int i,n;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+}
+
--- main.c
+++ main.c
@@ -1,28 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
-int sum();
+
 int main()
 {
-   /* FILE *pntr;
-    int x,sum=0;
-    pntr = fopen("C:\\Users\\MSIS\\Desktop\\n_numbers.txt","r+");
-    if(pntr == NULL)
-    {
-        printf("There are no numbers in the file");
-    }
-    else
-    {
-        printf("The file is now open");
-        while(!feof(pntr))
-        {
-            fscanf(pntr,"%d",&x);
-            sum+=x;
-
-        }
-        printf("\nThe sum is = %d ",sum);
-    }*/
-    sum();
-    sq();
+    printf("Hello world!\n");
     return 0;
 }
